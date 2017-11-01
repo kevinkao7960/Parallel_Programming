fb22933487bfd5a3061fb26a625732398e1bcd41
kevinkao7960 <kevinkao7960@gmail.com>
2017-11-01 17:06:12 +0800

    change data type of x, y

--- hw1/parallel.c
+++ hw1/parallel.c
@@ -12,7 +12,7 @@ void* thread_toss(void* rank);
 int main(int argc, char* argv[]){
     // calculate the program time
     clock_t start, end;
-    start = clock();
+    
     srand(time(NULL));
 
     // mutex initialize
@@ -26,7 +26,7 @@ int main(int argc, char* argv[]){
     thread_toss_num = number_of_tosses / thread_count;
 
     thread_handles = (pthread_t*)malloc(sizeof(pthread_t) * thread_count);
-
+    start = clock();
     for( thread = 0; thread < thread_count; thread++ ){
           pthread_create(&thread_handles[thread], NULL, thread_toss, NULL);
     }
@@ -35,32 +35,34 @@ int main(int argc, char* argv[]){
     for( thread = 0; thread < thread_count; thread++ ){
         pthread_join(thread_handles[thread], NULL);
     }
+    end = clock();
 
+    pthread_mutex_destroy(&mutex);
+    free(thread_handles);
     // calculate pi
     long double pi_estimate;
     pi_estimate = 4 * number_in_circle / ((long double)number_of_tosses);
 
-    end = clock();
+    
 
     printf("PI: %Lf\n", pi_estimate);
     double cpu_time_used = ((double)(end-start)) / CLOCKS_PER_SEC;
     printf("CPU Time: %f\n", cpu_time_used);
 
-    pthread_mutex_destroy(&mutex);
-    free(thread_handles);
+    
 
     return 0;
 }
 
 void* thread_toss(void* rank){
     long long int i;
-    long double x, y, distance_squared;
+    double x, y, distance_squared;
     // long long int my_n = number_of_tosses / thread_count;
     long long int tmp_sum = 0;
 
     for( i = 0; i < thread_toss_num; i++ ){
-        x = (long double)rand()/RAND_MAX*2.0 - 1.0;
-        y = (long double)rand()/RAND_MAX*2.0 - 1.0;
+        x = (double)rand()/RAND_MAX*2.0 - 1.0;
+        y = (double)rand()/RAND_MAX*2.0 - 1.0;
         distance_squared = x*x + y*y;
         if( distance_squared <= 1.0 ){
             tmp_sum++;
