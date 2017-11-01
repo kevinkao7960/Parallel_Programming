b0314902d80298b4909b01a4f871df28ca881866
kevinkao7960 <kevinkao7960@gmail.com>
2017-11-01 14:34:34 +0800

    global thread_toss_num

--- hw1/parallel.c
+++ hw1/parallel.c
@@ -5,7 +5,7 @@
 
 long long int thread_count;
 long long int number_in_circle = 0;
-long long int number_of_tosses;
+long long int thread_toss_num;
 pthread_mutex_t mutex;
 
 void* thread_toss(void* rank);
@@ -22,7 +22,8 @@ int main(int argc, char* argv[]){
     long thread;
     pthread_t* thread_handles;
     thread_count = atoi(argv[1]);
-    number_of_tosses = atoi(argv[2]);
+    long long int number_of_tosses = atoi(argv[2]);
+    thread_toss_num = number_of_tosses / thread_count;
 
     thread_handles = (pthread_t*)malloc(sizeof(pthread_t) * thread_count);
 
@@ -54,10 +55,10 @@ int main(int argc, char* argv[]){
 void* thread_toss(void* rank){
     long long int i;
     long double x, y, distance_squared;
-    long long int my_n = number_of_tosses / thread_count;
+    // long long int my_n = number_of_tosses / thread_count;
     long long int tmp_sum = 0;
 
-    for( i = 0; i < my_n; i++ ){
+    for( i = 0; i < thread_toss_num; i++ ){
         x = (long double)rand()/RAND_MAX*2.0 - 1.0;
         y = (long double)rand()/RAND_MAX*2.0 - 1.0;
         distance_squared = x*x + y*y;
