89de4410511b2a997e32601a6262dc463573c9f4
kevinkao7960 <kevinkao7960@gmail.com>
2017-11-28 19:57:20 +0800

    add some debug message3

--- hw3/integrate.c
+++ hw3/integrate.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
   /* var for MPI */
   int my_rank, size, source, dest = 0, tag = 0;
   long long int start, end, interval_amount;
-  long long local_sum;
+  double local_sum;
   MPI_Status status;
 
   MPI_Init(&argc, &argv);
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
     x_middle = (i - 0.5) * rect_width;
     area = sin(x_middle) * rect_width;
     local_sum = local_sum + area;
-    printf("for:rank:%d\tlocal_sum:%lld\n", my_rank, local_sum);
+    printf("for:rank:%dlocal_sum:%f\t\n", my_rank, local_sum);
   }
 
   if( my_rank == 0 ){
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
     printf("The total area is: %f\n", (float)sum);
   }
   else{
-    printf("rank:%d\tlocal_sum:%lld\n", my_rank, local_sum);
+    printf("rank:%d\tlocal_sum:%f\n", my_rank, (float)local_sum);
     MPI_Send(&local_sum, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
   }
   MPI_Finalize();
